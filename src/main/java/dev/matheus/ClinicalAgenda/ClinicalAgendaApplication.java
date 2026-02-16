a0b703fa906f2c06315a3570263ac385a1e34f31
matheus <mathgfmello@gmail.com>
2026-02-16 19:07:10 -0300

    feat: config do docker compose e application.yaml e inicializacao do projeto

---
+++ src/main/java/dev/matheus/ClinicalAgenda/ClinicalAgendaApplication.java
@@ -0,0 +1,13 @@
+package dev.matheus.ClinicalAgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheus/ClinicalAgenda/ClinicalAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheus.ClinicalAgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
