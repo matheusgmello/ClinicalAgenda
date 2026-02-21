31a9747770a5796f95f1ba2fa50a1dfff330a0c9
matheus <mathgfmello@gmail.com>
2026-02-21 11:50:54 -0300

    feat(core): validação de data para agendamento de consultas

--- src/main/java/dev/matheus/ClinicalAgenda/core/usecases/AgendaConsultaUseCaseImpl.java
+++ src/main/java/dev/matheus/ClinicalAgenda/core/usecases/AgendaConsultaUseCaseImpl.java
@@ -5,6 +5,8 @@ import dev.matheus.ClinicalAgenda.core.gateway.ConsultaGateway;
 import dev.matheus.ClinicalAgenda.infra.exceptions.ConsultaDataInvalidaException;
 import dev.matheus.ClinicalAgenda.infra.exceptions.ConsultaDuplicadaException;
 
+import java.time.LocalDateTime;
+
 public class AgendaConsultaUseCaseImpl implements AgendaConsultaUseCase {
 
     private final ConsultaGateway consultaGateway;
@@ -15,6 +17,9 @@ public class AgendaConsultaUseCaseImpl implements AgendaConsultaUseCase {
 
     @Override
     public Consulta execute(Consulta consulta) {
+        if (consulta.dataInicio().isBefore(LocalDateTime.now())) {
+            throw new ConsultaDataInvalidaException("Não é permitido agendar consultas para datas passadas.");
+        }
         if (consulta.dataInicio().isAfter(consulta.dataFim())) {
             throw new ConsultaDataInvalidaException("Data de início não pode ser posterior ao fim.");
         }
